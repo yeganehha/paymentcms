ff5f9198fda766bebc5c0d674d8bb0fee8f71b95
erfan ebrahimi <persionhost@gmail.com>
2019-05-09 20:02:03 +0430

    deactive plugin done

--- app/admin/controller/plugins.php
+++ app/admin/controller/plugins.php
@@ -33,7 +33,17 @@ class plugins extends \controller {
 	public function index(){
 		Response::redirect(\app::getBaseAppLink('plugins/lists'));
 	}
-	public function lists() {
+	public function lists($status = null,$file_name=null) {
+		if ( $status == 'appInstalled')
+			$this->alert('success' ,null ,rlang('appInstalled'));
+		elseif ($status == 'pleaseTryAGain')
+			$this->alert('danger' ,null ,rlang('pleaseTryAGain'));
+		elseif ($status == 'cantFindInfo')
+			$this->alert('danger' ,null ,rlang('cantFindInfo.php'). '<br>' . urldecode($file_name));
+		elseif ($status == 'appUninstalled')
+			$this->alert('success' ,null ,rlang('appUninstalled'));
+		elseif ($status == 'pleaseTryAGain')
+			$this->alert('danger' ,null ,rlang('pleaseTryAGain'));
 		$apps = \App::appsListWithConfig();
 		$this->mold->view('pluginLocalList.mold.html');
 		$this->mold->setPageTitle(rlang('plugins'));
@@ -58,29 +68,56 @@ class plugins extends \controller {
 						if ( model::queryUnprepared($query) ) {
 							model::commit();
 							$this->changeCacheOfAppStatus($app,'deActive');
-							$this->alert('success' ,null ,rlang('appInstalled'));
-							$this->lists();
+							Response::redirect(\app::getBaseAppLink('plugins/lists/appInstalled#app_'.$app));
 							return true;
 						}
 					} catch (Exception $exception){
 						model::rollback();
-						$this->alert('danger' ,null ,rlang('pleaseTryAGain'));
-						$this->lists();
+						Response::redirect(\app::getBaseAppLink('plugins/lists/pleaseTryAGain#app_'.$app));
 						return true;
 					}
 				} else {
 					$this->changeCacheOfAppStatus($app,'deActive');
-					$this->alert('success' ,null ,rlang('appInstalled'));
-					$this->lists();
+					Response::redirect(\app::getBaseAppLink('plugins/lists/appInstalled#app_'.$app));
 					return true;
 				}
 			} else {
-				$this->alert('danger', null, rlang('cantFindInfo.php') . '<br>' . $file_name);
-				$this->lists();
+				Response::redirect(\app::getBaseAppLink('plugins/lists/cantFindInfo/'.urlencode($file_name).'#app_'.$app));
 				return false;
 			}
 		} else {
 			$this->lists();
+			return true;
+		}
+	}
+
+	public function installing($app = null){
+		if ( is_null($app)){
+			Response::redirect(\app::getBaseAppLink('plugins/lists'));
+			return false;
+		}
+		$appStatus = cache::get('appStatus', $app ,'paymentCms');
+		if ( $appStatus == 'deActive' ){
+			$this->changeCacheOfAppStatus($app,'active');
+			Response::redirect(\app::getBaseAppLink('plugins/lists/appInstalled#app_'.$app));
+			return true;
+		} else {
+			Response::redirect(\app::getBaseAppLink('plugins/lists#app_'.$app));
+			return true;
+		}
+	}
+	public function deActive($app = null){
+		if ( is_null($app)){
+			Response::redirect(\app::getBaseAppLink('plugins/lists'));
+			return false;
+		}
+		$appStatus = cache::get('appStatus', $app ,'paymentCms');
+		if ( $appStatus == 'active' ){
+			$this->changeCacheOfAppStatus($app,'deActive');
+			Response::redirect(\app::getBaseAppLink('plugins/lists/appUninstalled#app_'.$app));
+			return true;
+		} else {
+			Response::redirect(\app::getBaseAppLink('plugins/lists#app_'.$app));
 			return true;
 		}
 	}
@@ -101,26 +138,22 @@ class plugins extends \controller {
 							model::commit();
 							file::removedir(payment_path.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.$app);
 							$this->changeCacheOfAppStatus($app,null);
-							$this->alert('success' ,null ,rlang('appUninstalled'));
-							$this->lists();
+							Response::redirect(\app::getBaseAppLink('plugins/lists/appUninstalled#app_'.$app));
 							return true;
 						}
 					} catch (Exception $exception){
 						model::rollback();
-						$this->alert('danger' ,null ,rlang('pleaseTryAGain'));
-						$this->lists();
+						Response::redirect(\app::getBaseAppLink('plugins/lists/pleaseTryAGain#app_'.$app));
 						return true;
 					}
 				} else {
 					file::removedir(payment_path.DIRECTORY_SEPARATOR.'app'.DIRECTORY_SEPARATOR.$app);
 					$this->changeCacheOfAppStatus($app,null);
-					$this->alert('success' ,null ,rlang('appInstalled'));
-					$this->lists();
+					Response::redirect(\app::getBaseAppLink('plugins/lists/appUninstalled#app_'.$app));
 					return true;
 				}
 			} else {
-				$this->alert('danger', null, rlang('cantFindInfo.php') . '<br>' . $file_name);
-				$this->lists();
+				Response::redirect(\app::getBaseAppLink('plugins/lists/cantFindInfo/'.urlencode($file_name).'#app_'.$app));
 				return false;
 			}
 		}
@@ -160,183 +193,6 @@ class plugins extends \controller {
 		$this->mold->set('apps' , $search);
 	}
 
-	private function checkBaseData(){
-		$form = request::post('id=0,name,description,price,link,active');
-		$rules = [
-			'id' => ['int|match:>=0'	, rlang('id')],
-			'name' => ['required'	, rlang('name')],
-			'description' => ['required'	, rlang('description')],
-			'price' => ['required|int|match:>=0', rlang('price')],
-			'link' => ['required'	, rlang('link')],
-		];
-		$valid = validate::check($form, $rules);
-		if ($valid->isFail()){
-			$this->alert('warning' , null,$valid->errorsIn(),'error');
-			$this->mold->set('post',$form);
-		} else {
-			/* @var \paymentCms\model\service $model */
-			if ( $form['id'] > 0 )
-				$model = $this->model('service' , $form['id']) ;
-			else
-				$model = $this->model('service') ;
-			$model->setPrice($form['price']);
-			$model->setDescription($form['description']);
-			$model->setLink($form['link']);
-			$model->setName($form['name']);
-			if( $form['active'] == 'active')
-				$model->setStatus(true);
-			else
-				$model->setStatus(false);
-			$status = false ;
-			if ( $form['id'] == 0 ) {
-				$action = 'insert';
-				$id = $model->insertToDataBase();
-				if ( $id > 0 )
-					$status = true ;
-			} else {
-				$status = $model->upDateDataBase();
-				$action = 'update';
-			}
-			if ($status ) {
-				Response::redirect(\app::getBaseAppLink('service/profile/' . $model->getServiceId() . '/'.$action.'ActionDone'));
-			} else {
-				$this->alert('warning' , null, rlang('pleaseTryAGain'),'error');
-				$this->mold->set('post',$form);
-			}
-		}
-	}
-
-	private function checkFieldData(){
-		$form = request::post('id,firstNameStatus=visible,lastNameStatus=visible,emailNameStatus=visible,phoneNameStatus=required,moreField,deleteField');
-		$rules = [
-			'id' => ['int|match:>0'	, rlang('id')],
-			'firstNameStatus' => ['format:{visible/invisible/required}'	, rlang(['status','firstName'])],
-			'lastNameStatus' => ['format:{visible/invisible/required}'	, rlang(['status','lastName'])],
-			'emailStatus' => ['format:{visible/invisible/required}'	, rlang(['status','email'])],
-			'phoneStatus' => ['format:{visible/invisible/required}'	, rlang(['status','phone'])],
-			'moreField.*.status' => ['format:{visible/invisible/required,admin}'	, rlang('status')],
-			'moreField.*.type' => ['format:{text/url/password/email/select/radio/checkbox/textarea/date/number/file}'	, rlang('type')],
-			'moreField.*.name' => ['notEmpty'	, rlang('name')],
-			'moreField.*.order' => ['number'	, rlang('orderToShow')],
-		];
-		$valid = validate::check($form, $rules);
-		if ($valid->isFail()){
-			$this->alert('warning' , null,$valid->errorsIn(),'error');
-			$this->mold->set('post',$form);
-		} else {
-			model::transaction();
-			/* @var \paymentCms\model\service $model */
-			/* @var \paymentCms\model\field $modelField */
-			$model = $this->model('service' , $form['id']) ;
-			$model->setFirstNameStatus($form['firstNameStatus']);
-			$model->setLastNameStatus($form['lastNameStatus']);
-			$model->setEmailStatus($form['emailNameStatus']);
-			$model->setPhoneStatus($form['phoneNameStatus']);
-			$status = $model->upDateDataBase();
-			if ($status ) {
-				if ( is_array($form['moreField']) and count($form['moreField']) > 0 )
-					foreach ($form['moreField'] as $key => $field ){
-					if ( $field['id'] > 0 )
-						$modelField = $this->model('field',$field['id']);
-					else
-						$modelField = $this->model('field');
-					$modelField->setStatus($field['status']);
-					$modelField->setDescription($field['description']);
-					$modelField->setOrder($field['order']);
-					$modelField->setRegex($field['regex']);
-					$modelField->setTitle($field['name']);
-					$modelField->setType($field['type']);
-					$modelField->setValues($field['value']);
-					$modelField->setServiceId($model->getServiceId());
-					if ( $field['id'] > 0 )
-						$modelField->upDateDataBase();
-					else
-						$modelField->insertToDataBase();
-				}
-				if ( is_array($form['deleteField']) and count($form['deleteField']) > 0 ){
-					$modelField->db()->where('fieldId' , $form['deleteField'] , 'IN');
-					$modelField->db()->where('serviceId' , $model->getServiceId() );
-					$modelField->db()->delete('field' );
-				}
-				model::commit();
-				Response::redirect(\app::getBaseAppLink('service/profile/' . $model->getServiceId() . '/moreConfigurationActionDone'));
-			} else {
-				model::rollback();
-				$this->alert('warning' , null, rlang('pleaseTryAGain'),'error');
-				$this->mold->set('post',$form);
-			}
-		}
-	}
-
-	public  function profile($serviceId , $action = null){
-		/* @var \paymentCms\model\service $model */
-		$model = $this->model('service' , $serviceId) ;
-		if ( $model->getServiceId() != $serviceId){
-			$this->mold->offAutoCompile();
-			\App\core\controller\httpErrorHandler::E404();
-			return ;
-		}
-		if ( request::isPost() ){
-			$_POST['id'] = $serviceId ;
-			$this->checkBaseData();
-			$this->mold->set('activeTab','edit');
-		}
-		if ( $action == 'updateActionDone' ){
-			$this->mold->set('activeTab','edit');
-			$this->alert('success',null,rlang(['edit','services','successfully','was']));
-		} elseif ( $action == 'insertActionDone' ){
-			$this->mold->set('activeTab','dashboard');
-			$this->alert('success',null,rlang(['add','services','successfully','was']));
-		} elseif ( $action == 'moreConfigurationActionDone' ){
-			$this->mold->set('activeTab','moreInfo');
-			$this->alert('success',null,rlang(['fields','services','successfully','was']));
-		}
-
-		/* @var \paymentCms\model\field $fieldModel */
-		$fieldModel = $this->model('field') ;
-		$fields = $fieldModel->search($serviceId,'serviceId = ? ' ,null,'*',['column'=>'orderNumber' , 'type' => 'desc' ]  );
-		if ( $fields === true)
-			$fields = [] ;
-		$this->mold->set('service',$model);
-		$this->mold->set('fields',$fields);
-		$this->mold->set('numberOfFields',count($fields));
-		$this->mold->view('serviceProfile.mold.html');
-		$this->mold->setPageTitle(rlang(['profile','services']). ': '.$model->getName());
-	}
-
-	public function editMore($serviceId){
-		/* @var \paymentCms\model\service $model */
-		$model = $this->model('service' , $serviceId) ;
-		if ( $model->getServiceId() != $serviceId){
-			$this->mold->offAutoCompile();
-			\App\core\controller\httpErrorHandler::E404();
-			return ;
-		}
-		if ( request::isPost() ){
-			$_POST['id'] = $serviceId ;
-			$this->checkFieldData();
-			$this->mold->set('activeTab','moreInfo');
-		} else
-			Response::redirect(\app::getBaseAppLink('service/profile/' . $model->getServiceId() ));
-
-
-		$this->mold->set('activeTab','moreInfo');
-		/* @var \paymentCms\model\field $fieldModel */
-		$fieldModel = $this->model('field') ;
-		$fields = $fieldModel->search($serviceId , 'serviceId = ?' , null ,'*',['column' => 'orderNumber' , 'type' => 'desc']);
-		if ( $fields === true)
-			$fields = [] ;
-		$this->mold->set('service',$model);
-		$this->mold->set('fields',$fields);
-		$this->mold->set('numberOfFields',count($fields));
-		$this->mold->view('serviceProfile.mold.html');
-		$this->mold->setPageTitle(rlang(['profile','services']). ': '.$model->getName());
-	}
-
-	private function generateAllAppsStatus(){
-		$links = model::searching(null, null, 'apps_link');
-		return cache::save($links, 'apps_link', PHP_INT_MAX, 'paymentCms');
-	}
 
 	private function generateQueryCreatTable($tables){
 		$query = '';
