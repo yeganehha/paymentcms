d542b9f4fe4a44a3a313fa17bc7d30805d3b259d
erfan ebrahimi <persionhost@gmail.com>
2019-05-25 17:19:01 +0430

    add gateway plugin

---
+++ plugins/gateway/lang/fa.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Yeganehha .
+ * User: Erfan Ebrahimi (http://ErfanEbrahimi.ir)
+ * Date: 3/26/2019
+ * Time: 5:50 PM
+ * project : paymentCMS
+ * virsion : 0.0.0.1
+ * update Time : 3/26/2019 - 5:50 PM
+ * Discription of this Page :
+ */
+
+
+if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
+
+
+return [
+	'fff' => '**fff**',
+];
