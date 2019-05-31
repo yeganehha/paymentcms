10fd6d19ba74a678e6932e7c5acae63f98be098e
erfan ebrahimi <persionhost@gmail.com>
2019-05-31 15:36:10 +0430

    cache inserted

---
+++ app/user/cache/data/115e355ad5115e6d09ea789fbd560ecc.php
@@ -0,0 +1,402 @@
+<?php
+	/* cache version 0.0.0.1 , created at : 2019-05-30 10:44:53
+		 from userPermissions */ 
+
+
+if ( ! function_exists('cacheSystem_afe23580206c6a5e645a531d80fe798e_checker' ) ) {
+
+	function cacheSystem_afe23580206c6a5e645a531d80fe798e_checker() {
+		return [ "dataSize" => 8311 , "version" => "0.0.0.1" , "time" => 3706689540 ]  ;
+	}
+
+	function cacheSystem_afe23580206c6a5e645a531d80fe798e_storage() {
+		return  [ 
+			 "0" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'api_service_index'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "1" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'api_service_info'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "2" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'api_invoice_generate'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "3" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'api_apps_listApps'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "4" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'api_update_version'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "5" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'core_httpErrorHandler_E404'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "6" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'core_httpErrorHandler_E500'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "7" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'core_httpErrorHandler_E403'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "8" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'home_home_index'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "9" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'home_home_checkData'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "10" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'invoice_home_index'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "11" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'invoice_invoice_generate'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "12" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'paymentServer_statistics_index'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "13" =>  [ 
+				 "user_groupId" =>  1  , 
+				 "accessPage" =>  'paymentServer_statistics_addStatistics'  , 
+				 "loginRequired" =>  0  , 
+				 ]  , 
+			 "14" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "15" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_pLists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "16" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "17" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_installLocal'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "18" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_installing'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "19" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_installingPlugin'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "20" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_deActivePlugin'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "21" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_deActive'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "22" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_uninstallLocal'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "23" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_uninstallLocalPlugin'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "24" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_plugins_install'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "25" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_service_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "26" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_service_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "27" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_service_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "28" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_service_profile'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "29" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_service_editMore'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "30" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_webservice_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "31" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_webservice_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "32" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_webservice_new'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "33" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_webservice_getIp'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "34" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_webservice_edit'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "35" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_linksConfiguration_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "36" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_linksConfiguration_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "37" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_linksConfiguration_edit'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "38" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_linksConfiguration_delete'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "39" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_eForms_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "40" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_eForms_all'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "41" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_eForms_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "42" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_eForms_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "43" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_eForms_edit'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "44" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_invoices_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "45" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_invoices_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "46" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_landingPage_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "47" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_landingPage_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "48" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_landingPage_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "49" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_landingPage_edit'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "50" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_permissions_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "51" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_permissions_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "52" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_permissions_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "53" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_permissions_edit'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "54" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_users_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "55" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_users_lists'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "56" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_users_insert'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "57" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'admin_users_profile'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "58" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'api_service_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "59" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'api_service_info'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "60" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'api_invoice_generate'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "61" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'api_apps_listApps'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "62" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'api_update_version'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "63" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'core_httpErrorHandler_E404'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "64" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'core_httpErrorHandler_E500'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "65" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'core_httpErrorHandler_E403'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "66" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'eForm_home_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "67" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'home_home_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "68" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'home_home_checkData'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "69" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'invoice_home_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "70" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'invoice_invoice_generate'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "71" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'landing_home_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "72" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'paymentServer_statistics_index'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "73" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'paymentServer_statistics_addStatistics'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "74" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'user_access_login'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "75" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'user_access_register'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 "76" =>  [ 
+				 "user_groupId" =>  2  , 
+				 "accessPage" =>  'user_access_logout'  , 
+				 "loginRequired" =>  1  , 
+				 ]  , 
+			 ]  ; 
+	}
+
+}
---
+++ core/cache/data/5f10aa94a396109203f80a928bc9a56a.php
@@ -0,0 +1,20 @@
+<?php
+	/* cache version 0.0.0.1 , created at : 2019-05-31 13:04:30
+		 from appStatus */ 
+
+
+if ( ! function_exists('cacheSystem_6831231c6cf49794a3b5d11fb0cf3700_checker' ) ) {
+
+	function cacheSystem_6831231c6cf49794a3b5d11fb0cf3700_checker() {
+		return [ "dataSize" => 79 , "version" => "0.0.0.1" , "time" => 3706784317 ]  ;
+	}
+
+	function cacheSystem_6831231c6cf49794a3b5d11fb0cf3700_storage() {
+		return  [ 
+			 "core" =>  'active'  , 
+			 "admin" =>  'active'  , 
+			 "user" =>  'active'  , 
+			 ]  ; 
+	}
+
+}
