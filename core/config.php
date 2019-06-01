d7e25e953712ae1f3c3af5ca67c7f0cf7fe0d064
erfan ebrahimi <persionhost@gmail.com>
2019-06-01 12:07:41 +0430

    return base config file

---
+++ core/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by Yeganehha .
+ * User: Erfan Ebrahimi (http://ErfanEbrahimi.ir)
+ * Date: 3/24/2019
+ * Time: 7:58 PM
+ * project : paymentCMS
+ * version : 0.0.0.1
+ * update Time : 3/24/2019 - 7:58 PM
+ * Description of this Page :
+ */
+
+if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
+
+
+return array('_dbHost' => 'localHost' , '_dbUsername' => 'root' , '_dbPassword' => '' , '_dbName' => 'payment1' , '_dbTableStartWith' => '');
