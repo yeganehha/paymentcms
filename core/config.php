2346f5ff516a3140d71728e28a7766547eb2ce52
erfan ebrahimi <persionhost@gmail.com>
2019-07-24 23:04:52 +0430

    config set with demo

--- core/config.php
+++ core/config.php
@@ -1,16 +1,5 @@
 <?php
-/**
- * Created by Yeganehha .
- * User: Erfan Ebrahimi (http://ErfanEbrahimi.ir)
- * Date: 3/24/2019
- * Time: 7:58 PM
- * project : paymentCMS
- * version : 0.0.0.1
- * update Time : 3/24/2019 - 7:58 PM
- * Description of this Page :
- */
-
-if (!defined('paymentCMS')) die('<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" type="text/css"><div class="container" style="margin-top: 20px;"><div id="msg_1" class="alert alert-danger"><strong>Error!</strong> Please do not set the url manually !! </div></div>');
+	/*  created at : 2019-06-20 15:34:27*/ 
 
 
-return array('_dbHost' => 'localHost' , '_dbUsername' => 'root' , '_dbPassword' => '' , '_dbName' => 'payment1' , '_dbTableStartWith' => '');
+return ['_dbHost' => 'localhost' , '_dbUsername' => 'paymentc_demo' , '_dbPassword' => 'Hz}gzB*[[snn' , '_dbName' => 'paymentc_demoV1.001' , '_dbTableStartWith' => 'per_'] ; 
